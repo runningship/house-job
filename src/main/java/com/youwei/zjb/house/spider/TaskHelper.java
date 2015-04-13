cfa2adb45df947e41f2599811f048668083f72bb
runningship <runningship@gmail.com>
2015-04-13 14:34:29 +0800

    more

--- src/main/java/com/youwei/zjb/house/spider/TaskHelper.java
+++ src/main/java/com/youwei/zjb/house/spider/TaskHelper.java
@@ -230,8 +230,10 @@ public class TaskHelper {
 			return "中装";
 		}else if(zxiu.contains("豪华装修")){
 			return "豪装";
+		}else if(zxiu.contains("毛坯")){
+			return "毛坯";
 		}else{
-			return zxiu.replace("装修情况：", "").replace("房", "");
+			return "";
 		}
 	}
 
@@ -273,9 +275,12 @@ public class TaskHelper {
 		if(StringUtils.isEmpty(text)){
 			return "无";
 		}
-		if(text.contains("限女性")){
+		if(text.contains("限女")){
 			return "限女性";
+		}else if(text.contains("限男")){
+			return "限男性";
+		}else{
+			return "男女不限";
 		}
-		return "男女不限";
 	}
 }
